f926edf409f83bbd1caec111ede70cb5ce68a7b1
olvera93 <olvera9302@outlook.com>
2022-11-26 15:03:50 -0600

    [ADD] CheckLocationPermissions

--- app/src/main/java/com/olvera/uberants/MainActivity.kt
+++ app/src/main/java/com/olvera/uberants/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.olvera.uberants
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import com.olvera.uberants.common.utils.PermissionUtils
 import com.olvera.uberants.databinding.ActivityMainBinding
 
 
@@ -16,6 +21,9 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var navController: NavController
+
+    private var permissionDenied = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,5 +41,58 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
             binding.toolbar.title = destination.label
             binding.toolbar.navigationIcon = null
         }
+
+        checkLocationPermissions()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun checkLocationPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION) ||
+            ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            PermissionUtils.RationaleDialog.newInstance(LOCATION_PERMISSION_REQUEST_CODE, true).show(supportFragmentManager, "dialog")
+            return
+        }
+
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            return
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION) ||
+            PermissionUtils.isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            checkLocationPermissions()
+        } else {
+            permissionDenied = true
+        }
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+
+        if (permissionDenied) {
+            showMissingPermissionsError()
+            permissionDenied = false
+        }
+    }
+
+    private fun showMissingPermissionsError() {
+        PermissionUtils.PermissionDeniedDialog.newInstance(true).show(supportFragmentManager, "dialog")
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 23
     }
 }
